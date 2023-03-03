728a104e9226c5fa43150d6dd43c2928ed937d02
ZAID <zaid.ghori1997@gmail.com>
2023-03-03 17:39:31 +0530

    adding laptops files too

---
+++ LeetCode/LeetCode/IsAlpha.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding
+{
+     class IsAlpha
+    {
+        //https://www.scaler.com/academy/mentee-dashboard/class/27558/assignment/problems/10694?navref=cl_tt_nv
+        public int solve(List<char> A)
+        {
+            foreach (var item in A)
+            {
+                if (! ((item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z')))
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/isAlNum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding
+{
+     class isAlNum
+    {
+        //https://www.scaler.com/academy/mentee-dashboard/class/27558/assignment/problems/10695?navref=cl_tt_nv
+        public int solve(List<char> A)
+        {
+            foreach (var item in A)
+            {
+                if (!((item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z') || (item >= '0' && item <= '9')))
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
