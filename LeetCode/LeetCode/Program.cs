60d2bbdcee59fa801d098cf193efaf86381d3eea
zdbz <33360958+zdbz@users.noreply.github.com>
2023-03-03 10:19:13 +0530

    Leetcode problems
    leetcode problems

---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+
+namespace LeetCode
+{
+    class A
+    {
+        private int a;
+    }
+    class Program : A
+    {
+        static void Main(string[] args)
+        {
+
+            #region hidden
+            //Solution solution = new Solution();
+            //PartitionArray partitionArray = new PartitionArray();
+            //int[] nums = new int[] { 1,3,5,7,9};
+            //int[] nums1 = new int[] {2,4,6,8};
+            //var region = "Europe";
+            //var name = "Belarus";
+            //string url = $"https://jsonmock.hackerrank.com/api/countries?region={region}&name={name}";
+            //WebRequest requestObject = WebRequest.Create(url);
+            //requestObject.Method = "GET";
+            //HttpWebResponse responseObject = (HttpWebResponse)requestObject.GetResponse();
+
+            //using (Stream stream =responseObject.GetResponseStream())
+            //{
+            //    StreamReader streamReader = new StreamReader(stream);
+            //    var obj = Json.parse(streamReader);
+            //    Console.WriteLine(streamReader.ReadToEnd());
+            //    streamReader.Close();
+            //}
+
+
+            //  Console.WriteLine(solution.PivotIndex(nums)); 
+            // Console.WriteLine(partitionArray.CanThreePartsEqualSum(nums));
+            //MedianTwoSortedArrays.FindMedianSortedArrays(nums, nums1);
+
+            //LinkedList l1 = new LinkedList();
+            //LinkedList l2 = new LinkedList();
+
+            //ListNode first = new ListNode(2);
+            //ListNode second = new ListNode(4);
+            //ListNode third = new ListNode(3);
+            //first.next = second;
+            //second.next = third;
+            //ListNode f2 = new ListNode(5);
+            //ListNode s2 = new ListNode(6);
+            //ListNode t3 = new ListNode(4);
+            //f2.next = s2;
+            //s2.next = t3;
+
+
+            //LinkedListAddTwoNumbers linkedlist = new LinkedListAddTwoNumbers();
+            //linkedlist.AddTwoNumbers(l1,l2); 
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
