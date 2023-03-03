2c1355d9da5fe80005e382ef9bfc5a5741316317
zdbz <33360958+zdbz@users.noreply.github.com>
2023-03-03 10:13:35 +0530

    Added files via upload.
    These are some leetcode problems that i have solved

---
+++ LeetCode/SumOfOddLengthSubarrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class SumOfOddLengthSubarrays
+    {
+        public int SumOddLengthSubarrays(int[] arr)
+        {
+            int arrSum = 0, mainSum=0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrSum += arr[i];
+            }
+            if(arr.Length % 2 != 0)
+            {
+                mainSum = 2 * arrSum;
+            }
+            else
+            {
+
+                //TODO
+            }
+            return mainSum;
+        }
+    }
+}
